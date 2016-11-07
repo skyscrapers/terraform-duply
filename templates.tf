6dd40fd5c5d40ea8d80a0ca3e20db352628ebcad
Ringo De Smet <ringo@de-smet.name>
2016-11-07 16:02:12 +0100

    Using the ARN of the bucket.

--- templates.tf
+++ templates.tf
@@ -1,6 +1,6 @@
 data "template_file" "duply" {
     template = "${file("${path.module}/templates/duply-policy.tpl")}"
     vars {
-        arn = "${var.arn}"
+        arn = "${aws_s3_bucket.duply.arn}"
     }
 }
