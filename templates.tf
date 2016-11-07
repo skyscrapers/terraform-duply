30877ddea32edf388bd5b11febbabafd55f6a232
Philip Dubois <heazky@gmail.com>
2016-11-07 15:04:46 +0100

    Initial commit

---
+++ templates.tf
@@ -0,0 +1,6 @@
+data "template_file" "duply" {
+    template = "${file("${path.module}/templates/duply-policy.tpl")}"
+    vars {
+        arn = "${var.arn}"
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "customer" {
+  default = ""
+}
+
+variable "project" {
+  default = ""
+}
+
+variable "environment" {
+  default = ""
+}
